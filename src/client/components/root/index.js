93848ae559bc9f746c8e027b64f5193b26b902fd
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-02-10 00:34:11 +0100

    feat(style): Asana like list

--- src/client/components/root/index.js
+++ src/client/components/root/index.js
@@ -14,16 +14,21 @@ function Component(taskList) {
       const bySections = _.groupBy(this.props.taskList.tasks, 'assignee_status')
       const sections = ['inbox', 'today', 'upcoming']
       return (
-        <div>
-          <h1>Tasks</h1>
-          {sections.map(section =>
-            <section key={section}>
-              <h2>{section}</h2>
-              <ul>
-                {(bySections[section]||[]).map(task => <li key={task.id}>{task.name}</li>)}
-              </ul>
-            </section>
-          )}
+        <div className="app__container">
+          <h1 className="app__title">Asana Global View</h1>
+          <div className="app__content">
+            <main className="tasks">
+              {sections.map(section =>
+                <section key={section} className="section">
+                  <h2 className="section__title">{section}</h2>
+                  <ol>
+                    {(bySections[section]||[]).map(task => <li className={"section__task" + (task.name[task.name.length - 1] === ':' ? ' heading' : '')} key={task.id}>{task.name}</li>)}
+                  </ol>
+                </section>
+              )}
+            </main>
+            <aside className="tags"></aside>
+          </div>
         </div>
       )
     }
